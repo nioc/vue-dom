2e138a4cb24fb695af29fbda81eca0d04db5ca5d
nioc <dev@nioc.eu>
2021-01-09 16:42:06 +0100

    Display rooms as tree in admin

---
+++ src/components/admin/RoomsTree.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="room in rooms" :key="room.id">
+      <router-link :to="{name: 'admin-room', params: {id: room.id}}" :class="{'has-text-danger': !room.isVisible}">{{ room.name }}</router-link>
+      <rooms-tree :rooms="room.childs" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'RoomsTree',
+  props: {
+    rooms: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
