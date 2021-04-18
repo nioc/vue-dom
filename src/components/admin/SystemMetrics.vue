ed18087861d4bd096c7920e89975ea5269605f29
nioc <dev@nioc.eu>
2021-04-18 23:18:06 +0200

    Display system health

--- src/components/admin/SystemMetrics.vue
+++ src/components/admin/SystemMetrics.vue
@@ -12,14 +12,32 @@
     <section class="card-content">
       <b-loading v-model="isLoading" :is-full-page="false" />
 
-      <div v-if="metrics.load" class="field mb-5">
+      <div v-if="health" class="field mb-5">
+        <div class="control">
+          <label class="label">État de santé</label>
+          <div class="mb-3">
+            <span class="is-flex is-justify-content-space-between">
+              <span title="">Etat global</span>
+              <span class="icon"><i class="fa" :class="[health.healthy ? 'fa-check has-text-success' : 'fa-times has-text-danger']" /></span>
+            </span>
+          </div>
+          <div v-for="component in health.components" :key="component.key" class="mb-3">
+            <span class="is-flex is-justify-content-space-between">
+              <span>{{ component.label }}</span>
+              <span class="icon"><i class="fa" :class="[component.status ? 'fa-check has-text-success' : 'fa-times has-text-danger']" /></span>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="metrics && metrics.load" class="field mb-5">
         <div class="control">
           <label class="label">Charge processeur ({{ metrics.cpuCores }} cores)</label>
           <system-metric v-for="load in metrics.load" :key="load.period" :label="`Moyenne sur ${load.period}`" :value="load.value" :max="metrics.cpuCores" />
         </div>
       </div>
 
-      <div v-if="metrics.memory" class="field mb-5">
+      <div v-if="metrics && metrics.memory" class="field mb-5">
         <div class="control">
           <label class="label">Mémoire</label>
           <system-metric label="RAM" :human-value="`${getHumanSize(metrics.memory.memFree)} disponible (extensible à ${getHumanSize(metrics.memory.memAvailable)}) sur ${getHumanSize(metrics.memory.memTotal)}`" :value="metrics.memory.memTotal-metrics.memory.memAvailable" :max="metrics.memory.memTotal" />
@@ -27,14 +45,14 @@
         </div>
       </div>
 
-      <div v-if="metrics.disk" class="field mb-5">
+      <div v-if="metrics && metrics.disk" class="field mb-5">
         <div class="control">
           <label class="label">Espace disque</label>
           <system-metric v-for="fs in metrics.disk" :key="fs.fs" :label="fs.mount" :label-title="fs.fs" :human-value="`${getHumanSize(fs.available)} disponible sur ${getHumanSize(fs.size)}`" :value="fs.size-fs.available" :max="fs.size" />
         </div>
       </div>
 
-      <div v-if="metrics.date" class="has-text-weight-light is-italic mb-3">Informations collectées <time-ago v-if="metrics.date" :drop-fixes="false" :date="metrics.date" :title="metrics.date | moment('LLL')" /></div>
+      <div v-if="metrics && metrics.date" class="has-text-weight-light is-italic mb-3">Informations collectées <time-ago v-if="metrics.date" :drop-fixes="false" :date="metrics.date" :title="metrics.date | moment('LLL')" /></div>
 
       <span class="buttons">
         <button class="button is-primary" title="Rafraichir l'état" @click="getSystemMetrics">
@@ -60,8 +78,8 @@ export default {
   data () {
     return {
       isLoading: false,
-      metrics: {
-      },
+      metrics: null,
+      health: null,
     }
   },
   methods: {
@@ -69,6 +87,11 @@ export default {
       this.isLoading = true
       try {
         this.metrics = await this.$Provider.getSystemMetrics()
+      } catch (error) {
+        this.$store.commit('app/setInformation', { type: 'is-danger', message: error.message })
+      }
+      try {
+        this.health = await this.$Provider.getSystemHealth()
       } catch (error) {
         this.$store.commit('app/setInformation', { type: 'is-danger', message: error.message })
       }
