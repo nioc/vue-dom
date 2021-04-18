0249fde849eeb6f51efbc512c9c79ef1d3afdbf5
nioc <dev@nioc.eu>
2021-04-18 17:21:50 +0200

    Enhance room tiles UX

--- src/components/RoomTile.vue
+++ src/components/RoomTile.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link :to="{name: 'room', params: {id: room.id}}" class="column" style="min-width: max-content">
-    <div class="notification  p-5 is-primary has-text-centered">
-      <span class="subtitle">{{ room.name }}</span>
+    <div class="notification p-5 is-primary has-text-centered is-fullheight">
+      <span class="subtitle is-block is-inline-mobile mb-0">{{ room.name }}</span>
       <span class="is-size-7">
         <span v-for="info in summary.keys" :key="info.key" class="pl-2"><i class="fa fa-fw" :class="getSummaryIconClass(info.key)" />{{ info.value }}{{ getSummaryUnit(info.key) }}</span>
       </span>
