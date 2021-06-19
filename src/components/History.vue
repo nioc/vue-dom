a1a40be87bc84e616160ccb8856db5fbfab337dd
nioc <dev@nioc.eu>
2021-06-20 01:34:00 +0200

    Redesign history form

--- src/components/History.vue
+++ src/components/History.vue
@@ -2,10 +2,27 @@
   <div>
     <div class="field is-horizontal">
       <div class="field-body">
-        <div class="field" title="Date de début">
+        <div class="field is-narrow" title="Durée d'historique">
+          <div class="control has-icons-left">
+            <div class="select is-small">
+              <select v-model="duration" @change="updateDates">
+                <option value="PT1H">1 heure</option>
+                <option value="PT6H">6 heures</option>
+                <option value="PT24H">24 heures</option>
+                <option value="P7D">7 jours</option>
+                <option value="P30D">30 jours</option>
+                <option :value="null">Personnalisé</option>
+              </select>
+              <div class="icon is-small is-left">
+                <i class="fas fa-history" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-if="duration === null" class="field" title="Date de début">
           <b-datetimepicker v-model="startDate" :max-datetime="today" size="is-small" />
         </div>
-        <div class="field" title="Date de fin">
+        <div v-if="duration === null" class="field" title="Date de fin">
           <b-datetimepicker v-model="endDate" :max-datetime="today" size="is-small" />
         </div>
         <div class="field">
@@ -41,6 +58,7 @@ export default {
     return {
       startDate: null,
       endDate: null,
+      duration: 'PT24H',
       history: null,
       loaded: false,
     }
@@ -67,6 +85,12 @@ export default {
         labels: history.map((point) => point.date),
       }
       this.loaded = true
+    },
+    updateDates () {
+      if (this.duration !== null) {
+        this.startDate = this.$moment().subtract(this.$moment.duration(this.duration)).toDate()
+        this.getHistory()
+      }
     },
   },
 }
