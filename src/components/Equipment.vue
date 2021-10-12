256135cf51bd29cb99ddcfc2aec01d2e190621a9
nioc <dev@nioc.eu>
2021-10-13 00:44:45 +0200

    Add link to admin equipment

--- src/components/Equipment.vue
+++ src/components/Equipment.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="card mb-4">
     <header class="card-header">
-      <p class="card-header-title">{{ equipment.name }}</p>
+      <p class="card-header-title">{{ equipment.name }}<router-link v-if="hasRole('admin')" :to="{name: 'admin-equipment', params: {id: equipment.id}}" title="Gérer l'équipement"><i class="fas ml-2 fa-sm fa-tools has-text-grey-light" /></router-link></p>
       <aside class="card-header-icon is-size-7-mobile">
         <span v-if="equipment.battery" :class="[equipment.hasLowBattery ? 'has-text-danger' : 'has-text-grey']"><i class="fa-mr" :class="getBatteryLevelIconClass(equipment.battery)" />{{ equipment.battery }}%</span>
         <span v-if="equipment.lastCommunication" :title="equipment.lastCommunication | moment('LLLL')" :class="[equipment.hasNoCommunication ? 'has-text-danger' : 'has-text-grey']" class="ml-2"><i class="fa-mr far fa-clock" /><time-ago :date="equipment.lastCommunication" :drop-fixes="true" /></span>
@@ -20,6 +20,7 @@ import { createNamespacedHelpers } from 'vuex'
 import { CmdMixin } from '@/mixins/Cmd'
 const custom = window.custom
 const { mapGetters } = createNamespacedHelpers('data')
+const { mapState } = createNamespacedHelpers('app')
 
 export default {
   components: {
@@ -37,6 +38,7 @@ export default {
   computed: {
     equipment () { return this.getEquipmentById(this.id) },
     ...mapGetters(['getEquipmentById']),
+    ...mapState(['roles']),
   },
   methods: {
     getEquipmentComponent (moduleName) {
@@ -44,6 +46,9 @@ export default {
     },
     isEquipmentHandled (moduleName) {
       return (custom.components[moduleName] !== undefined)
+    },
+    hasRole (role) {
+      return this.roles.includes(role)
     },
   },
 }
