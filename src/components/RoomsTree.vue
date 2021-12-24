c02aac19fe772d09b8bb41a98186bfdd00606812
nioc <dev@nioc.eu>
2021-12-24 23:03:06 +0100

    Fix rooms tree

--- src/components/RoomsTree.vue
+++ src/components/RoomsTree.vue
@@ -3,7 +3,7 @@
     <li v-for="room in rooms" :key="room.id" class="has-medium-tag">
       <router-link :to="{name: 'room', params: {id: room.id}}" class="tag is-medium is-primary p-5" :title="`Consulter la pièce &quot;${room.name}&quot;`">
         <span class="has-text-weight-medium">{{ room.name }}</span>
-        <span v-if="room.summary && room.summary.length > 0" class="is-size-7">
+        <span class="is-size-7">
           <span v-for="info in getRoomSummaryById(room.id).keys" :key="info.key" class="pl-2"><i class="fa fa-fw" :class="getSummaryIconClass(info.key)" />{{ info.value }}{{ getSummaryUnit(info.key) }}</span>
         </span>
 
