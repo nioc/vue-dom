9a3d756dfc1c323a89bd85c8d665072329e1867a
nioc <dev@nioc.eu>
2023-07-08 18:47:40 +0200

    feat(navbar): Change icons order

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,13 +18,13 @@
             <router-link v-for="userView in dataStore.activeUserViews" :key="userView.code" :to="{name: 'view', params: {code: userView.code}}" class="navbar-item"><span><i class="fa-fw fa-mr" :class="userView.icon" />{{ userView.title }}</span></router-link>
           </div>
         </div>
-        <router-link class="navbar-item" :to="{name: 'scenarios'}" title="Scénarios"><i class="fa fa-book fa-fw fa-mr" /><span class="is-navbar-label">Scénarios</span></router-link>
         <div v-if="dataStore.tagsList.length" class="navbar-item has-dropdown" :class="{'is-active': hasDropdownTagsDisplayed}" @click="hasDropdownTagsDisplayed = !hasDropdownTagsDisplayed">
           <router-link v-slot="{href, isActive}" :to="{name: 'tags'}" custom><a :href="href" class="navbar-link is-arrowless" :class="{'router-link-active': isActive}" @click.prevent=""><i class="fa fa-tags fa-fw fa-mr" /><span class="is-navbar-label">Catégories</span></a></router-link>
           <div class="navbar-dropdown">
             <router-link v-for="tag in dataStore.tagsList" :key="tag" :to="{name: 'tag', params: {tag}}" class="navbar-item"><span>{{ tag }}</span></router-link>
           </div>
         </div>
+        <router-link class="navbar-item" :to="{name: 'scenarios'}" title="Scénarios"><i class="fa fa-book fa-fw fa-mr" /><span class="is-navbar-label">Scénarios</span></router-link>
         <router-link v-if="authStore.hasRole('admin')" class="navbar-item" :to="{name: 'admin'}" title="Admin"><i class="fa fa-tools fa-fw fa-mr" /><span class="is-navbar-label">Admin</span></router-link>
       </div>
       <div class="navbar-end is-flex-center">
